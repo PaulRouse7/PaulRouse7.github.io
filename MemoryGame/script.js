e85411115cb699d532e430c433a202631f9f9150
Paul <paul-rouse@hotmail.com>
2023-09-12 22:46:14 -0400

    Added memory game

--- MemoryGame/script.js
+++ MemoryGame/script.js
@@ -13,49 +13,31 @@ const COLORS = [
   "purple"
 ];
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
+
 function shuffle(array) {
   let counter = array.length;
-
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
 let shuffledColors = shuffle(COLORS);
 
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
+
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
-    // create a new div
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
+
 
 let attempts = 0;
 const cardsPicked = [];
@@ -86,14 +68,14 @@ function handleCardClick(event) {
         cardsPicked[1].classList.remove('flipped');
         cardsPicked[0].style.backgroundColor = '';
         cardsPicked[1].style.backgroundColor = '';
-        cardsPicked.length = 0; // Clear the array
-        noClicking = false; // Enable clicking again
+        cardsPicked.length = 0; 
+        noClicking = false;
       }, 1000);
     }
   }
 }
 
-
-  
-// when the DOM loads
 createDivsForColors(shuffledColors);
+
+
+
