1a3bee9979e6085392cac95c2f0b412fbf060931
Paul <paul-rouse@hotmail.com>
2023-10-01 13:27:51 -0400

    Fruit Search Project

---
+++ Fruit-Search-Project/script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+function search(str) {        
+	let results = [];
+	for(let fruits of fruit){
+		let bigFruit = fruits.toUpperCase();
+		if(bigFruit.includes(str) && str !== ''){
+			results.push(fruits);
+		}
+	}
+	return results;
+}
+
+input.addEventListener('keyup',searchHandler);           
+function searchHandler(e) {
+	suggestions.innerHTML = '';
+	let results = search(e.target.value.toUpperCase());  
+	for(let result of results){
+		let fruit = document.createElement('li');
+		fruit.innerText = result;
+		suggestions.appendChild(fruit);
+	}
+}
+
+
+function showSuggestions(results, inputVal) {           
+	searchHandler();
+	search();
+}
+
+suggestions.addEventListener('click',useSuggestion)
+function useSuggestion(e) {              
+	let fruit = e.target.innerText;
+	input.value = fruit;
+}
